b0038195e068bdb236cbafb37ea206028905de87
Ewen Lukacik <newelukacik7360@gmail.com>
2024-12-13 17:56:46 +0100

    Advancements on tuto symfony : form

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,30 +2,70 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Recipe;
+use App\Form\RecipeType;
 
 class RecipeController extends AbstractController
 {
+    /**
+     * @param Request
+     * @param RecipeRepository
+     */
     #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $recipeRepository): Response
     {
-       return $this->render('recipe/index.html.twig');
+        $recipes = $recipeRepository->findWithDurationLowerthan(120);
+        
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
     }
 
+    /**
+     * @param Request
+     * @param RecipeRepository
+     * @param string slug
+     * @param int id
+     */
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $recipeRepository ): Response
     {
+        $recipe = $recipeRepository->find($id);
+
+        if($recipe->getSlug() != $slug){
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
-            'person' => [
-                'firstname' => 'John',
-                'lastname' => "Doe"
-            ]
+            'recipe' => $recipe
+        ]);
+    }
+
+    /**
+     * @param Request
+     * @param Recipe
+     * @param EntityManagerInterface
+     */
+    #[Route('/recette/{id}/edit', name: 'recipe.edit', requirements: ['id' => '\d+'])]
+    public function edit(Request $request, Recipe $recipe, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+            $this->addFlash('success', 'La recette a bien été modifiée');
+            return $this->redirectToRoute('recipe.index');
+        }
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form
         ]);
     }
 }
